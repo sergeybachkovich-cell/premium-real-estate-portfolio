6a5556f06580e12cc4f8fe912ec50c6d5fa31950
Сергей <sergeybachkovich@gmail.com>
2026-05-15 19:54:23 +0300

    fix deploy

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -10,7 +10,7 @@ const __dirname = path.dirname(__filename);
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), viteSingleFile()],
-  base: '/premium-real-estate-portfolio/',
+  base: '/tolkiHandMade/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
